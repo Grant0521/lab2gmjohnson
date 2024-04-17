fe39ed92f8abf9d0b1e12616f792ea15bc24860a
ethansy878 <esychangco@gmail.com>
2024-04-17 14:48:22 -0700

    fix namespace error on card.cxx

--- card.cxx
+++ card.cxx
@@ -7,26 +7,28 @@
 using namespace groupGE;
 
 // Stand-alone Card definitions
-std::ostream& operator<<(std::ostream& os, const Card &c) {
-    std::string faces[4] = {"J", "Q", "K", "A"};
-    if (c.rank <= 10){
-        os << c.rank;
+namespace groupGE{
+    std::ostream& operator<<(std::ostream& os, const Card &c) {
+        std::string faces[4] = {"J", "Q", "K", "A"};
+        if (c.rank <= 10){
+            os << c.rank;
+        }
+        else {
+            os << faces[c.rank - 11];
+        }
+
+        // 1, 2, 3, 4
+        std::string suits[4] = {"♠", "♥", "♦", "♣"};
+        os << suits[c.suit - 1];
+
+        return os;
     }
-    else {
-        os << faces[c.rank - 11];
+
+    Card::operator int() const {
+        return suit * rank;
     }
 
-    // 1, 2, 3, 4
-    std::string suits[4] = {"♠", "♥", "♦", "♣"};
-    os << suits[c.suit - 1];
-
-    return os;
-}
-
-Card::operator int() const {
-    return suit * rank;
-}
-
-bool operator< (const Card &c1, const Card &c2) {
-    return int(c1) < int(c2);
+    bool operator< (const Card &c1, const Card &c2) {
+        return int(c1) < int(c2);
+    }
 }
