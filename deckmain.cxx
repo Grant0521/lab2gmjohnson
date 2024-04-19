6f065247918bfa5708e0a0bbefd2180df08c842f
ethansy878 <esychangco@gmail.com>
2024-04-18 19:23:23 -0700

    update deck test to test 3 decks

--- deckmain.cxx
+++ deckmain.cxx
@@ -15,13 +15,15 @@ using namespace groupGE;
 int main(void) {
     Deck deckCards;
     Card tempCard;
-    cout << "[Decktest] Printing a full deck! (Shuffled at 5 cards remaining)" << endl;
-    for (int row = 0; row < 4; row ++){
-        for (int i = 0; i < 13; i++) {
-            tempCard = deckCards.deal();
-            cout << tempCard << " ";
-        }    
-        cout << endl;
+    cout << "[Decktest] Printing 3 full decks! (Shuffled at 5 cards remaining)" << endl;
+    for (int deck = 0; deck < 3; deck++){
+        for (int row = 0; row < 4; row ++){
+            for (int i = 0; i < 13; i++) {
+                tempCard = deckCards.deal();
+                cout << tempCard << " ";
+            }    
+            cout << endl;
+        }
     }
     return EXIT_SUCCESS;
 }
